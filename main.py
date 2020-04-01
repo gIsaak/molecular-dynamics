4bbd614fec8dd5f2ba56a57d491b23c54a33e301
Ludwig <l.hendl@student.tudelft.nl>
2020-04-01 19:42:08 +0000

    Added diffusion arrays to save values to be able to generate plots as shown in the report. Added the option to save the data in binary file.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import simulation_func_vector as fu
+import diffusion_processor as diff
+import h5py
 
 ##### User-populated parameters/arrays for batch simulations #####
 T_085= [0.591,0.658,0.719,0.760,0.782,0.786,0.880,1.128,1.214,2.202,2.889,3.000,3.500,4.174]
@@ -6,10 +8,17 @@ T_045 = [1.55, 1.71, 2.93, 4.62]
 T_010 = [5.5,6.5,7.5]
 rho = 0.45
 
+# Initialisation of arrays for diffusion calculation
+# must be individually adjusted to the loop calling the main function
+ite = 20 # number of iterations to average diffusion curve
+d_saver_10 = np.zeros(shape=(10000)) # shape = number_of_timesteps
+diffErr = np.zeros(shape=(ite))
+
 C = []
 dC = []
 P = []
 dP = []
+
 
 for t in T_045: 
     MDS_parameters = {
@@ -34,8 +43,24 @@ for t in T_045:
     'save_figures':         False,   # Flag to automatically save generated figures
     
     }
-    avgP,Perr,avgC,Cerr = fu.main(MDS_parameters)
+    avgP,Perr,avgC,Cerr,D,Derr = fu.main(MDS_parameters)
     C.append(avgC)
     dC.append(Cerr)
     P.append(avgP)
     dP.append(Perr)
+    d_saver_10 += D
+    diffErr[t] = Derr
+
+# optional saving of data in binary file
+#hf = h5py.File('simulation_add_parameter_info.h5', 'w')
+#hf.create_dataset('spec_heat', data=C)
+#hf.create_dataset('spec_heat_err', data=dc)
+#hf.create_dataset('pressure', data=P)
+#hf.create_dataset('pressure_err', data=dp)
+#hf.create_dataset('diffusion', data=d_saver_10)
+#hf.create_dataset('diffusion_err', data=diffErr)
+#hf.close()
+
+# funtion to plot diffusion for 3 differnt diffusion data sets
+# it is adviseable to work in diffusion_processor seperately to generate the plots
+#values = diff.plot_diffusion(G,GErr,L,LErr,S,SErr)
